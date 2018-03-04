f37002a7c6f10b99c652dea850fe712a7ee0264b
Gustavo Oliveira <gusoliv3ira@gmail.com>
2018-03-04 00:02:26 -0300

    adicionando a biblioteca particles.js

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "<h1>Olá mundo!<h1>"
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT",5000))
